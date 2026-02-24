c9569204fd88b79af1da769768848c3a756c344f
Jordi Saumell <jsaumell420@gmail.com>
2026-02-24 09:28:06 +0100

    Add some code animations

--- composeApp/src/commonMain/kotlin/com/bcnkode/meetup/errorhandling/Eh15RaiseContextParameters.kt
+++ composeApp/src/commonMain/kotlin/com/bcnkode/meetup/errorhandling/Eh15RaiseContextParameters.kt
@@ -6,25 +6,20 @@ import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.bcnkode.meetup.Sizes
-import com.bcnkode.meetup.layouts.TitleAndContentScaffold
 import com.bcnkode.meetup.composables.CodeInPaneWithTitle
+import com.bcnkode.meetup.layouts.TitleAndContentScaffold
 import net.kodein.cup.Slide
-import net.kodein.cup.sa.SourceCode
-import net.kodein.cup.sa.SourceCodeThemes
+import net.kodein.cup.sa.SAStyle
+import net.kodein.cup.sa.line
 import net.kodein.cup.sa.rememberSourceCode
 import net.kodein.cup.speaker.SpeakerNotes
 import org.jetbrains.compose.resources.DrawableResource
@@ -36,27 +31,30 @@ val raiseContextParametersErrorHandling by Slide(
             
         """.trimIndent()
     ),
-    stepCount = 3,
+    stepCount = 8,
 ) { step ->
 
     val contextParameters = rememberSourceCode(language = "kotlin") {
+        val log by marker(onlyShown(3..4))
+        val raise by marker(onlyShown(4))
             """
 interface Logger {
     internal fun log(level: LogLevel, group: String?, message: String)
 }
-context(logger: Logger) 
+${log}context(logger: Logger) 
 fun log(level: LogLevel, group: String?, message: String) =
-    logger.log(level, group, message)
+    logger.log(level, group, message)${X}
   
   
-context(_: Raise<Error>) 
-fun validatePerson(name: String, age: Int): Person
+${raise}context(_: Raise<Error>) 
+fun validatePerson(name: String, age: Int): Person${X}
 """.trimIndent()
         }
 
     val raiseWithContextsCode = rememberSourceCode(language = "kotlin") {
+        val highlight by marker(highlighted(6))
         """
-context(_: Raise<ValidationError>, _: Raise<ApiError>)
+context(_: Raise<${highlight}ValidationError${X}>, _: Raise<${highlight}ApiError${X}>)
 fun registerUser(email: String, pass: Int) {
     ensure(pass.length >= 8) { TooShortPassword }
     ensure(email.contains("@")) { InvalidEmail }
@@ -69,15 +67,10 @@ fun Api.createUser(email: String, pass: Int): User = ...
     }
 
     val raiseUtilsCode = rememberSourceCode(language = "kotlin") {
+        val errorStyle = SAStyle.line(Color.Red, squiggle = true, through = false)
+        val error by marker(styled(errorStyle, 1))
         """
-fun Raise<OrderError>.findById(userId: Int): User =
-    ensureNotNull(db.findById(userId)) { UserNotFound(userId) }
-    
-fun Raise<OrderError>.processPayment(order: Order) {
-    ensure(Buy in order.user.permissions) { NotBuyPermission }
-    if (order.price > balance) raise(OrderError.InsufficientFunds)
-    // calculate
-}
+fun ${error}Raise<OrderError>.Raise<OrderError>${X}.myMethod() {...}
 """.trimIndent()
     }
 
@@ -86,10 +79,12 @@ fun Raise<OrderError>.processPayment(order: Order) {
         subtitle = "Context Parameters improve Raise",
         contentAlignment = Alignment.Center,
     ) {
-        when (step) {
-            0 -> CodeInPaneWithTitle("What about multiple errors?", raiseUtilsCode)
-            1 -> CodeInPaneWithTitle("New Kotlin feature: Context Parameters (experimental)", contextParameters)
-            2 -> CodeInPaneWithTitle("We can declare multiple error types!", raiseWithContextsCode)
+        Column {
+            when (step) {
+                0,1 -> CodeInPaneWithTitle("What about multiple errors?", raiseUtilsCode, step = step)
+                2,3,4 -> CodeInPaneWithTitle("New Kotlin feature: Context Parameters (experimental)", contextParameters, step = step)
+                5,6,7 -> CodeInPaneWithTitle("We can declare multiple error types!", raiseWithContextsCode, step = step)
+            }
         }
     }
 }
