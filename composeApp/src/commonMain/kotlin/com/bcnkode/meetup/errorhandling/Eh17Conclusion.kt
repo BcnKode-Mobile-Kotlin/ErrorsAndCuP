217280fcbffaa0c057e28ea37687a3e43d4fe605
Jordi Saumell <jordi.saumell@basetis.com>
2026-02-24 17:17:29 +0100

    Add some fade in animations

--- composeApp/src/commonMain/kotlin/com/bcnkode/meetup/errorhandling/Eh17Conclusion.kt
+++ composeApp/src/commonMain/kotlin/com/bcnkode/meetup/errorhandling/Eh17Conclusion.kt
@@ -2,6 +2,7 @@ package com.bcnkode.meetup.errorhandling
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -14,6 +15,7 @@ import com.bcnkode.meetup.MyStyleSheet.unaryMinus
 import com.bcnkode.meetup.MyStyleSheet.unaryPlus
 import com.bcnkode.meetup.composables.CodeInPaneWithTitle
 import com.bcnkode.meetup.layouts.TitleAndContentScaffold
+import com.bcnkode.meetup.utils.showIf
 import net.kodein.cup.Slide
 import net.kodein.cup.sa.rememberSourceCode
 import net.kodein.cup.speaker.SpeakerNotes
@@ -25,7 +27,7 @@ val conclusionErrorHandling by Slide(
             
         """.trimIndent()
     ),
-    stepCount = 1,
+    stepCount = 4,
 ) { step ->
 
     val simpleUnionCode = rememberSourceCode(language = "kotlin") {
@@ -44,18 +46,29 @@ fun fetchUser(userId: Int): User | NoInternet | ServerError | UserNotFound
         title = "Conclusion",
         contentAlignment = Alignment.Center,
     ) {
-        Column {
+        Column(Modifier.fillMaxWidth()) {
 
             Spacer(Modifier.weight(2f))
-            Text("Each tool requires more effort in learning, but it helps a bit more")
+            Text(
+                "Each tool has an additional learning curve, and some payoff",
+                Modifier.showIf(step > 0),
+            )
             Spacer(Modifier.height(8.dp))
-            Text("Try-catch → MyResult → Raise", style = MaterialTheme.typography.bodyMedium)
+            Text(
+                "Try-catch → MyResult → Raise",
+                Modifier.showIf(step > 0),
+                style = MaterialTheme.typography.bodyMedium,
+            )
             Spacer(Modifier.weight(1f))
-            Text("How far should we get, depends on each team")
+            Text("How far we should get depends on each team", Modifier.showIf(step > 1))
             Spacer(Modifier.weight(1f))
-            Text("MyResult vs try-catch has a bigger effect than it may seem")
+            Text("MyResult vs try-catch has a bigger impact than it may seem", Modifier.showIf(step > 2))
             Spacer(Modifier.height(8.dp))
-            Text(styled(MyStyleSheet) { "Like ${+code}val${-code} vs ${+code}final X${-code}: in Kotlin we use immutability much more than in java" }, style = MaterialTheme.typography.bodyMedium)
+            Text(
+                styled(MyStyleSheet) { "Like ${+code}val${-code} vs ${+code}final X${-code}: in Kotlin we use immutability much more than in java" },
+                Modifier.showIf(step > 2),
+                style = MaterialTheme.typography.bodyMedium,
+            )
             Spacer(Modifier.weight(2f))
         }
     }
